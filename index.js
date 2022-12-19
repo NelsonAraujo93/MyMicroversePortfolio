f187a6fa06edfcb89436b14be27f0d1fe958346b
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2022-12-19 10:41:30 -0500

    Add open and close menu function on previous weeks, fix ESlinters

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const init =()=>{
-    const openBtn = document.getElementById('open-menu-btn');
-    const closeBtn = document.getElementById('close-menu-btn');
-    const menuContainer = document.getElementById('menu-container');
+const init = () => {
+  const openBtn = document.getElementById('open-menu-btn');
+  const closeBtn = document.getElementById('close-menu-btn');
+  const menuContainer = document.getElementById('menu-container');
 
+  openBtn.addEventListener('click', () => {
+    menuContainer.style.display = 'flex';
+    openBtn.style.display = 'none';
+  });
 
+  closeBtn.addEventListener('click', () => {
+    menuContainer.style.display = 'none';
+    openBtn.style.display = 'block';
+  });
+};
 
-    openBtn.addEventListener('click',()=>{
-        menuContainer.style.display = 'flex';
-        openBtn.style.display= 'none';
-    })
-
-    closeBtn.addEventListener('click',()=>{
-        menuContainer.style.display = 'none';
-        openBtn.style.display= 'block';
-    })
-
-
-    console.log('lets go')
-}
 init();
